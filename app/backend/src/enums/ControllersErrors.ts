9305a112933e08652ace1deff8e1c26d0ec7a34c
Matheus Henrique Gonzaga <matheushg@pop-os.localdomain>
2022-08-07 18:56:42 -0300

    feat(mongo-controller) Create a ControllersErros enum

---
+++ app/backend/src/enums/ControllersErrors.ts
@@ -0,0 +1,9 @@
+enum ControllersErrors {
+  internal = 'Internal Server Error',
+  notFound = 'Object not found',
+  requiredId = 'Id is required',
+  badRequest = 'Bad request',
+  idLength = 'Id must have 24 hexadecimal characters',
+}
+
+export default ControllersErrors;
