b93c73b5e0bd8d1058be387b5fe32fa8283c1e1d
Matheus Henrique Gonzaga <matheushg@pop-os.localdomain>
2022-08-07 19:18:43 -0300

    feat(server) Initialize car controller and route in server

--- app/backend/src/server.ts
+++ app/backend/src/server.ts
@@ -1,5 +1,13 @@
 import App from './app';
+import CustomRouter from './Routes/Router';
+import CarsController from './controllers/CarController';
+import { Car } from './interfaces/CarInterface';
 
 const server = new App();
+
+const carsController = new CarsController();
+const CarRouter = new CustomRouter<Car>();
+CarRouter.addRoute(carsController);
+server.addRouter(CarRouter.router);
 
 export default server;
